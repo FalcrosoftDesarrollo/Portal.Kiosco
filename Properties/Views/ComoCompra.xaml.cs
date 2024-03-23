e941b9dd2c913a144395de10f6d276ff236e841c
StivenHerrera20 <stiven3722@gmail.com>
2024-03-22 23:01:03 -0500

    Update

---
+++ Properties/Views/ComoCompra.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Portal.Kiosco.Properties.Views
+{
+    /// <summary>
+    /// Lógica de interacción para ComoCompra.xaml
+    /// </summary>
+    public partial class ComoCompra : Window
+    {
+        public ComoCompra()
+        {
+            InitializeComponent();
+        }
+    }
+}
