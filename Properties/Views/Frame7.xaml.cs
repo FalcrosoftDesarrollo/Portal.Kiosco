f2b046f786ecbdf0a0a797a1c78ece383cf9f721
FalcrosoftDesarrollo <164076619+FalcrosoftDesarrollo@users.noreply.github.com>
2024-03-26 17:21:17 -0500

    Add files via upload

---
+++ Properties/Views/Frame7.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Portal.Kiosco.Properties.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Sala.xaml
+    /// </summary>
+    public partial class Frame7 : Page
+    {
+        public Frame7()
+        {
+            InitializeComponent();
+        }
+    }
+}
