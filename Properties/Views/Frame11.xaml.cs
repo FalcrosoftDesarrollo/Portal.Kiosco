460205c7b367f22bcb9efb78222da7386aef162d
FalcrosoftDesarrollo <164076619+FalcrosoftDesarrollo@users.noreply.github.com>
2024-03-27 17:15:50 -0500

    Frames Antoine

---
+++ Properties/Views/Frame11.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Portal.Kiosco.Properties.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Frame11.xaml
+    /// </summary>
+    public partial class Frame11 : Page
+    {
+        public Frame11()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Properties/Views/Frame9.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Portal.Kiosco.Properties.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Frame9.xaml
+    /// </summary>
+    public partial class Frame9 : Page
+    {
+        public Frame9()
+        {
+            InitializeComponent();
+        }
+    }
+}
