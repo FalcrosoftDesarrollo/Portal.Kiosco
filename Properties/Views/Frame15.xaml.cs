40ebfd2593a9dd4e19872e6258ff9dc0e29576e3
Stiven Herrera <112430585+StivenHerrera20@users.noreply.github.com>
2024-03-28 14:09:54 -0500

    Update

---
+++ Properties/Views/Frame15.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Portal.Kiosco.Properties.Views
+{
+    /// <summary>
+    /// Lógica de interacción para Frame15.xaml
+    /// </summary>
+    public partial class Frame15 : Window
+    {
+        public Frame15()
+        {
+            InitializeComponent();
+        }
+    }
+}
