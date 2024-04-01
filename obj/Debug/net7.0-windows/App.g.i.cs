f8249e9935640a5e3c0cdadb2cd241401e1848de
twaannn <73antocol@gmail.com>
2024-04-01 09:33:49 -0500

    Commit Antoine

--- obj/Debug/net7.0-windows/App.g.i.cs
+++ obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Portal.Kiosco {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Portal.Kiosco {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public static void Main() {
             Portal.Kiosco.App app = new Portal.Kiosco.App();
             app.InitializeComponent();
